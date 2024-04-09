0544e7ab891a0d1f80a74707eeb1a79f56214a33
BiniyamDS <biniyam.yr@gmail.com>
2024-04-09 13:15:41 +0300

    added functionality to fetch top 10 most starred repos

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
-
+const baseURL = "https://api.github.com/search/repositories";
+const filterURL = "&sort=stars&order=desc&per_page=10";
 app.use(express.static("./public"));
 // app.use(express.urlencoded({ extended: false }));
 
 app.get("/api/stars", (req, res) => {
-  const { start, stop } = req.query;
-  console.log(start, stop);
-  
-  res.json({ success: true, data: [] });
+  const { start, end } = req.query;
+  const fullURL = `${baseURL}?q=created:${start}..${end}${filterURL}`;
+  const getRepos = async () => {
+    try {
+      const response = await axios.get(fullURL);
+      const { items } = response.data;
+      let res_data = [];
+      items.forEach((element) => {
+        console.log(element.name, element.stargazers_count);
+        res_data.push({ title: element.name, count: element.stargazers_count });
+      });
+      res.json({ success: true, data: res_data });
+    } catch (err) {
+      console.log(`Error: ${err}`);
+    }
+  };
+  getRepos();
 });
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
